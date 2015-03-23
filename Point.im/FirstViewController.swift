f2b971433cc871879dbbb0b0d3e78c05f664250b
Alxander Borunov <borunov@yandex.ru>
2015-03-23 09:07:18 +0500

    Initial Commit

---
+++ Point.im/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Point.im
+//
+//  Created by Александр Борунов on 23.03.15.
+//  Copyright (c) 2015 Александр Борунов. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
